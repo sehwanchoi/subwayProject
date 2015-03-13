b1afb643ec61533684ef8a6acbfae2d8beaa6df3
Linda Kung <lindakung417@gmail.com>
2015-03-12 22:16:54 -0400

    fixed the server and can now access subway status. removed register route bc it was not doing anything

--- server/app/index.js
+++ server/app/index.js
@@ -1,28 +1,23 @@
 'use strict';
-var path = require('path');
 var express = require('express');
 var app = express();
-module.exports = app;
 
+module.exports = app;
 require('./configure')(app);
 
 app.use('/api', require('./routes'));
 
-app.use(function (req, res, next) {
+app.get('/*', function (req, res) {
 
-    if (path.extname(req.path).length > 0) {
-        res.status(404).end();
-    } else {
-        next(null);
+    if (req.xhr) {
+        return res.status(404).send('You are probably looking for something that starts with /api.');
     }
 
-});
-
-app.get('/*', function (req, res) {
     res.sendFile(app.get('indexHTMLPath'));
+
 });
 
 // Error catching endware.
 app.use(function (err, req, res, next) {
-    res.status(err.status).send(err.message);
+    res.status(err.status).send({ error: err.message });
 });
--- server/app/routes/register/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var router = require('express').Router(),
-    mongoose = require('mongoose');
-
-module.exports = router;
-
-router.get('/register', function(req, res, next) {
-    console.log("hello");
-});
