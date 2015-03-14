175d8b1f5663fb6542778091b5e61e5758b30cac
Jisan Zaman <jzaman@wesleyan.edu>
2015-03-14 15:18:29 -0400

    have logic implemented for this

--- browser/js/common/factories/mtaFactory.js
+++ browser/js/common/factories/mtaFactory.js
@@ -1,18 +1,15 @@
 'use strict';
 
 app.factory('mtaFactory', function($http, $q, $timeout) {
-    return {
-        getServiceInfo: function() {
-        	var deferred = $q.defer();
-        	$timeout(function() {
-        		deferred.resolve($http.get('/api/mta').then(function(response) {
-        			return response.data
-        		}))
-        	}, 3000);
-        	return deferred.promise;
-        	}
-        }
-     });
-            // return $http.get('/api/mta').then(function(response) {
-            //     return response.data;
-            // })
+  return {
+    getServiceInfo: function() {
+      var deferred = $q.defer();
+
+      deferred.resolve($http.get('/api/mta').then(function(response) {
+        return response.data
+      }))
+
+      return deferred.promise;
+    }
+  }
+});
