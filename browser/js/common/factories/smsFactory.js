4c5067cac18db657409de4f88dfecbac1109b8fa
Linda Kung <lindakung417@gmail.com>
2015-03-14 19:06:28 -0400

    fixed a bracket thing

--- browser/js/common/factories/smsFactory.js
+++ browser/js/common/factories/smsFactory.js
@@ -3,7 +3,7 @@
 app.factory('smsFactory', function($http) {
   return {
     sendSMS: function(message) {
-      return $http.post('/api/sms', {message}).then(function(response) {
+      return $http.post('/api/sms', message).then(function(response) {
         return response.data;
       });
     }
