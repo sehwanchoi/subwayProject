7184c389521fe44fe73e05688fd3c93a7369eab0
Linda Kung <lindakung417@gmail.com>
2015-03-16 11:07:45 -0400

    Fixed styling on register page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -10,23 +10,23 @@ app.config(function ($stateProvider) {
 app.controller('homeCtrl', function($scope, $rootScope, $timeout, mtaFactory, AuthService, AUTH_EVENTS, $state) {
 
 
-		$scope.serviceStatus = 
 			mtaFactory.getServiceInfo().then(function(status) {
 			$scope.groupOne = [];
-			var status1 = status.splice(0,5);
+			$scope.groupTwo = [];
+			
+			var status1 = status.slice(0,5);
 				$scope.groupOne.push(status1);
 				$scope.groupOne = status1;
+			
+			var status2 = status.slice(5,10);
+			$scope.groupTwo.push(status2);
+			$scope.groupTwo = status2;
 		})
 
 
 
-		mtaFactory.getServiceInfo().then(function(status) {
-			$scope.groupTwo = [];
-			var status2 = status.splice(5,9);
-			$scope.groupTwo.push(status2);
-			$scope.groupTwo = status2;
-		})	
-
+		
+		
 		var setUser = function () {
                 AuthService.getLoggedInUser().then(function (user) {
                     scope.user = user;
